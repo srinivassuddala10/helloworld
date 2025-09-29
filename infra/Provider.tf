84c4098141c34c22fe449aaa8ccf101262537580
srinivassuddala10 <srinivassuddala10@gmail.com>
2025-09-29 19:43:49 +0530

    Update Provider.tf

--- infra/Provider.tf
+++ infra/Provider.tf
@@ -9,5 +9,6 @@ terraform {
 
 provider "aws" {
   region  = "us-east-1"
-  profile = "973508469163"
+  access_key = "AKIA6FKNAMGV4S7E3O7I"
+  secret_key = "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"
 }
